94e79c683b1af7af238f55a21d3b1f541a781bc0
joaogabriellyra <jgabriellyra@hotmail.com>
2024-11-18 03:47:43 -0300

    finalizado html e css do header

---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+import { MapPin, ShoppingCart } from '@phosphor-icons/react'
+import { CoffeeDeliveryLogo } from './ui/coffee-delivery-logo'
+
+export function Header() {
+  return (
+    <header>
+      <div className="py-8 px-40 flex justify-between items-center">
+        <CoffeeDeliveryLogo />
+        <div className="flex items-center gap-3">
+          <div className="flex items-center p-2 gap-1 rounded-md bg-[#EBE5F9]">
+            <MapPin size={22} className="text-[#8047F8]" weight="fill" />
+            <span className="text-[#4B2995]">Recife, PE</span>
+          </div>
+          <div className="p-2 bg-[#F1E9C9] rounded-md">
+            <ShoppingCart size={22} weight="fill" className="text-[#C47F17]" />
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}

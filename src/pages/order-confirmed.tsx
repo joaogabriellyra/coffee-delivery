1d1c59790413fa986add00e954b94b141e64e690
joaogabriellyra <jgabriellyra@hotmail.com>
2024-12-13 17:24:44 -0300

    add order-confirmed page and route

---
+++ src/pages/order-confirmed.tsx
@@ -0,0 +1,7 @@
+export function OrderConfirmed() {
+  return (
+    <div>
+      <h1>HELLO MY FRIEND</h1>
+    </div>
+  )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/home'
 import { Checkout } from './pages/checkout'
+import { OrderConfirmed } from './pages/order-confirmed'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/checkout" element={<Checkout />} />
+      <Route path="/order-confirmed" element={<OrderConfirmed />} />
     </Routes>
   )
 }
